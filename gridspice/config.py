9030418eac157851b10179e5a51ef3666b14f0d6
Jimmy Du <jimmydu@dn5221de.sunet>
2012-07-12 18:11:18 -0700

    pydev project

--- gridspice/config.py
+++ gridspice/config.py
@@ -1,6 +1,6 @@
 DEFAULT_TIMEZONE = "PST"
 DEFAULT_DATE = "2000-01-01 00:00:00"
 DEFAULT_CLIMATE = "los angeles, ca"
-URL = "http://127.0.0.1:57372/"
+URL = "http://127.0.0.1:57848/"
 DEFAULT_MODULE_NAMES = ["Power Flow", "Residential", "Market", "Wholesale",
             "Network", "Generators"]
