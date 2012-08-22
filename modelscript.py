74cd3df7c6e24ddc04c41bff54beebb7d934da6e
Jimmy Du <jimmydu@rescomp-11-208562.stanford.edu>
2012-08-21 22:44:38 -0700

    editting modelscript

--- modelscript.py
+++ modelscript.py
@@ -12,8 +12,8 @@ lat = 37.449
 long = -122.1960
 for x in range(1, 4):
 	nod = element.powerflow.powerflow_object.node()
-	nod.latitude = repr(lat + x)
-	nod.longitude = repr(long + x)
+	nod.latitude = repr(lat + x*0.001)
+	nod.longitude = repr(long + x*0.001)
 	nod.name = 'node-' + repr(x)
 	mod.add(nod)
 mod.save()
