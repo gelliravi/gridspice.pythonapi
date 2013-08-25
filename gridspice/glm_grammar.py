7ff65a8ae1fc9fe533b8dc9eb1a48c6512724704
Jimmy Du <jimmydu@unknown28cfdad99378.gateway.2wire.net>
2013-08-24 18:21:18 -0700

    Successful parsing of clock block

--- gridspice/glm_grammar.py
+++ gridspice/glm_grammar.py
@@ -8,6 +8,7 @@ __status__ = "Development"
 
 from pyparsing import *
 
+# Lexemes
 MODULE = Literal('module')
 OBJECT = Literal('object')
 CLOCK = Literal('clock')
@@ -18,18 +19,34 @@ NEW_LINE = Literal('\n')
 OPEN_BRACKET = Literal('{')
 CLOSE_BRACKET = Literal('}')
 POUND = Literal('#')
+APOSTROPHE = Literal("'")
+DASH = Literal('-')
+COLON = Literal(':')
+PLUS = Literal('+')
 
-word = Word(alphas + "'.")
+word = Word(alphas)
 number = Word(nums)
-unit = word
-attribute = word
+
+# Clock properties
+date = Word(nums, exact=4) + DASH + Word(nums, exact=2) + DASH + Word(nums, exact=2) 
+time = Word(nums, max=2) + COLON + Word(nums, exact=2) + COLON + Word(nums, exact=2) 
+timestamp = APOSTROPHE + date + time + APOSTROPHE
+timezone = word + PLUS + number + word 
+
+identifier = Group(number)
+unit = Group(word)
+attribute = Group(word)
 comment = Group(COMMENT_START + SkipTo(NEW_LINE))
-value = Group((word | number) + Optional(unit))
+value = Group(((word | number) + Optional(unit)) ^ timestamp ^ timezone)
+
+# Property block
 property_ = Group(attribute + value + SEMI_COLON)  
-properties = Group(OneOrMore(property_)) # Currently no support for nested objects
+properties = Group(OPEN_BRACKET + OneOrMore(property_) + CLOSE_BRACKET) # Currently no support for nested objects
 properties_block = Group(OPEN_BRACKET + properties + CLOSE_BRACKET)
+
+# File Blocks
 module_block = Group(MODULE + word + Optional(properties_block) + SEMI_COLON)
-object_block = Group(OBJECT + word + COLON + number + properties)
+object_block = Group(OBJECT + word + Optional(COLON + identifier) + properties)
 clock_block = Group(CLOCK + properties)
 macro = Group(POUND + SkipTo(NEW_LINE))
 glm_file = Group(OneOrMore(comment | module_block | object_block | clock_block | macro))
