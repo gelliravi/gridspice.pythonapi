4b8a3406445d2c5e1ed24bf94d5e03c46f2e74a7
Jimmy Du <jimmydu@unknown28cfdad99378.gateway.2wire.net>
2013-09-02 13:08:23 -0700

    Made all fixes possible, sans pyparsing module's bug

--- gridspice/glm_grammar.py
+++ gridspice/glm_grammar.py
@@ -29,33 +29,33 @@ word = Word(alphas)
 number = Word(nums)
 
 # Clock properties
-date = Combine(Word(nums, exact=4) + DASH + Word(nums, exact=2) + DASH + Word(nums, exact=2)).setResultsName('date') 
-time = Combine(Word(nums, max=2) + COLON + Word(nums, exact=2) + COLON + Word(nums, exact=2)).setResultsName('time')
-timestamp = Group((APOSTROPHE + date + time + APOSTROPHE).setParseAction(lambda s: s[1:-1])).setResultsName('timestamp')
-timezone = Combine(word + PLUS + number + word).setResultsName('timezone') 
+date = Combine(Word(nums, exact=4) + DASH + Word(nums, exact=2) + DASH + Word(nums, exact=2))('date') 
+time = Combine(Word(nums, max=2) + COLON + Word(nums, exact=2) + COLON + Word(nums, exact=2))('time')
+timestamp = Group(APOSTROPHE + date + time + APOSTROPHE)('timestamp')
+timezone = Combine(word + PLUS + number + word)('timezone') 
 
-identifier = number.setResultsName('identifier')
-unit = word.setResultsName('unit')
-name = Word(alphas+nums+'-_*(,.):+/,"').setResultsName('name')
-attribute = name.setResultsName('attribute')
-comment = Group(COMMENT_START + SkipTo(NEW_LINE)).setResultsName('comment')
-value = (Group(name + Optional(unit)) ^ timestamp ^ timezone).setResultsName('value')
-object_type = Word(alphas+'_').setResultsName('type')
-identity = (COLON + identifier).setParseAction(lambda s: s[1:])
+identifier = number('identifier')
+unit = word('unit')
+name = Word(alphas+nums+'-_*(,.):+/,"')('name')
+attribute = name('attribute')
+comment = Group(COMMENT_START + SkipTo(NEW_LINE))('comment')
+value = (Group(name('value_name') + Optional(unit)) ^ timestamp ^ timezone)('value')
+object_type = Word(alphas+'_')('type')
+identity = (COLON + identifier)
 
 # Property block
-property_ = (attribute + value + SEMI_COLON).setParseAction(lambda s: s[:-1]).setResultsName('properties', listAllMatches=True)  
+property_ = Group(attribute + value + SEMI_COLON).setResultsName('properties') #List  
 properties = Forward()
 
 # File Blocks
-module_block = Group((MODULE + name + Optional(properties) + SEMI_COLON).setParseAction(lambda s,l,t: t[1:-1])).setResultsName('modules', listAllMatches=True)
-object_block = Group((OBJECT + object_type + Optional(identity) + properties).setParseAction(lambda s,l,t: t[1:])).setResultsName('objects', listAllMatches=True)
-recorder_block = Group((OBJECT + RECORDER + Optional(identity) + properties + SEMI_COLON).setParseAction(lambda s,l,t: t[1:-1])).setResultsName('recorders', listAllMatches=True)
-clock_block = Group((CLOCK + properties).setParseAction(lambda s,l,t: t[1:])).setResultsName('clock')
-macro = Group((POUND + SkipTo(NEW_LINE)).setParseAction(lambda s: s[1:])).setResultsName('macros', listAllMatches=True)
+module_block = Group(MODULE + name('module_name') + Optional(properties) + SEMI_COLON).setResultsName('modules') #List
+object_block = Group(OBJECT + object_type + Optional(identity) + properties).setResultsName('objects') #List
+recorder_block = Group(OBJECT + RECORDER + Optional(identity) + properties + SEMI_COLON).setResultsName('recorders') #List
+clock_block = Group(CLOCK + properties).setResultsName('clock')
+macro = Group(POUND + SkipTo(NEW_LINE)('macro_content')).setResultsName('macros') #List
 glm_file = ZeroOrMore(module_block ^ object_block ^ clock_block ^ macro ^ recorder_block) + stringEnd
 
 # Initialization
-properties << (OPEN_BRACKET + OneOrMore(property_ | object_block) + CLOSE_BRACKET).setParseAction(lambda s: s[1:-1])
+properties << (OPEN_BRACKET + OneOrMore(property_ | object_block) + CLOSE_BRACKET)
 glm_file.ignore(comment)
 
